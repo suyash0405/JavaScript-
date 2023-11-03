044c8328c642e6d91f05b4d0493bfbba7a613a90
Suyash Chaudhary <suyash0405@gmail.com>
2023-11-03 16:45:25 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "suyash123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountId =  2
+accountEmail = "asdk@gmail.com"
+accountPassword ="324234"
+accountCity = "jaipur"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  // trear all JS code as newer version
+
+// alert(3+4) // we are using node js not browser
+
+console.log(3+3);
+
+console.log("Suyash Chaudhary")
+
+let name = "Aarush"
+let age = 2
+let isLoggedIn = false
+let state = null;
+
+
+//number => 2 to power 53
+//bigint 
+//string 
+//boolean => true / false
+//null =>  stand alone value  , it's a representation of empty value
+//undefined = > when value is not assign 
+//symbol => unique 
+
+// ---- Non premitive 
+// object
+
+console.log(typeof "Meera")
+console.log(typeof age)
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
