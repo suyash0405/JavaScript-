8a8c7a039e4cb9de5b3e37af947375875020829d
Suyash Chaudhary <suyash0405@gmail.com>
2023-11-04 05:29:49 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "suyash";
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// ture =>1
+
+let isLoggedIn = "abc"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true
+// "" => false
+//"abc" => true
+
+let someNumber = 13
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
