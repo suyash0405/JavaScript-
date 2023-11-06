154e038b0554a4c6db30162955b9bde927f94b5b
Suyash Chaudhary <suyash0405@gmail.com>
2023-11-06 11:58:05 +0000

    scopes

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,42 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHeroes = ["shakitman", "ram"]
+
+const myArr2 = new Array(1,2,3,4,5,6)
+
+// console.log(myArr[2]);
+
+//arrays methods
+
+// myArr.push(8)
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join()
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+//slice ,splice
+
+// console.log("A ", myArr);
+// const myn1 = myArr.slice(1,3) // not removed from original array
+// console.log(myn1)
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1,3) //remove from original array
+// console.log(myn2)
+// console.log(" ", myArr);
+
+
+
+
+
+
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+// let a = 10
+// const b = 20
+// var c = 30
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var c = 300
+let a = 100
+if (true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log("Inner: ",a);
+
+}
+
+
+
+console.log("outer:", a);
+// console.log(b);
+// console.log(c);
