fdda85a628948900c467b1687f5b64e491b7226e
Suyash Chaudhary <suyash0405@gmail.com>
2023-11-16 07:27:23 +0000

    Math PI

---
+++ 10_classes_and_oops/mathpi.js
@@ -0,0 +1,36 @@
+// console.log(Math.PI)
+// Math.PI = 4
+// console.log(Math.PI) //cannot change
+
+//=============
+
+// const descripter = Object.getOwnPropertyDescriptor(Math,"PI")
+
+// console.log(descripter)
+//==============
+
+const chai ={
+    name : "ginger chai",
+    price : 250,
+    isAvailable :true,
+
+    orderChai :function(){
+        console.log("chai nahi h")
+
+    }
+}
+// console.log(chai)
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+Object.defineProperty(chai,"name",{
+    //  writable :false,
+    enumerable :false
+
+})
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+
+for (let [key,value] of  Object.entries(chai)){
+    
+    if (typeof value != "function"){
+        console.log(`${key} : ${value}`)
+    }
+}
